e8b093a02e8c35fb61f21f094e02779476e416b1
hhan87 <hhan87@gmail.com>
2019-02-24 01:36:10 +0900

    adjust gitignore
    add simple parser

---
+++ Assets/Scripts/StoryModule.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryModule : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        var sscp = new StoryScriptParser();
+        sscp.test();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/StoryScriptParser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+public class StoryScriptParser
+{
+    public void test()
+    {
+        var sdt = new StoryDataTest();
+        string json =  sdt.getTestJson();
+        Debug.Log(json);
+        var sdtData = JObject.Parse(json);
+        //Debug.Log(sdtData.ToString());
+        var s = sdtData["data"][0]["MainStory001_start"]["title"].Value<string>();
+        Debug.Log(s);
+        //var jsons = new JObject();
+        //jsons.Add("type", "test");
+        //jsons.Add("number", 124f);
+        //Debug.Log(json.ToString());
+    }
+}
