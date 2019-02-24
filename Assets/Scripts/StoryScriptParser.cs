02e3e4e3a174ed684acf99efc576b85c48602b77
hhan87 <hhan87@gmail.com>
2019-02-25 00:32:10 +0900

    add component

--- Assets/Scripts/StoryScriptParser.cs
+++ Assets/Scripts/StoryScriptParser.cs
@@ -9,14 +9,21 @@ public class StoryScriptParser
     {
         var sdt = new StoryDataTest();
         string json =  sdt.getTestJson();
-        Debug.Log(json);
+        //Debug.Log(json);
         var sdtData = JObject.Parse(json);
         //Debug.Log(sdtData.ToString());
-        var s = sdtData["data"][0]["MainStory001_start"]["title"].Value<string>();
+        var s = sdtData["data"][0]["MainStory001_start"]["data"].Values();
         Debug.Log(s);
+        foreach ( JToken j in s)
+        {
+            //Debug.Log(j.); 
+            Debug.Log(j["NpcNameText"].Value<string>());
+        }
+        
         //var jsons = new JObject();
         //jsons.Add("type", "test");
         //jsons.Add("number", 124f);
         //Debug.Log(json.ToString());
     }
 }
+
