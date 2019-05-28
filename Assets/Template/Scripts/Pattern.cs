0e194d8b549bd8f348063ab6e1685bece695d177
kalmmis <basronyu@gmail.com>
2019-05-29 00:37:28 +0900

    Pattern.cs
    struggling on new patterns

--- Assets/Template/Scripts/Pattern.cs
+++ Assets/Template/Scripts/Pattern.cs
@@ -28,9 +28,27 @@ public class Pattern : ScriptableObject
             case "A" :
                 parent.transform.Translate(Vector3.down * speed * Time.deltaTime);
                 break;
-            case "B":
-                parent.transform.Translate(Vector3.down * speed * Time.deltaTime);
-                break;
+            case "B":                 GameObject target = GameObject.Find("Player");                 //플레이어가 없으면 쫒아갈 것도 없음                 if (target == null) { Destroy(target); return; };
+                Vector3 v = new Vector3(0, 0, 0);
+                    if (target != null && v.Equals(new Vector3(0, 0, 0)))
+                    {
+                        /*                          항상 백터크기를 1로 하는 방법                          기울기a 는 a=(y2-y1)/(x2-x1);                          vector의 크기를 항상 n로 하기 위해서는 위 ax=y에서 x^2+y^2 = n^2이 되어야한다.                          n=1 일때를 기준으로                           Math.Sqrt = 제곱근 구하는 함수.                          x1 = (float) Math.Sqrt(Math.Abs(1/(a*a+1)));                          y1 = (float) Math.Sqrt(Math.Abs(a*a/(a*a+1)));                          이된다.                          한편 x1과 y1은 절대값이므로 초기 x와 y의 양수/음수 구분을 기록해 뒀다가 마지막에 양수음수를 맞춰줘야한다.                          */
+                        Transform targetTransform = target.GetComponent<Transform>();
+                        Transform parentTransform = parent.GetComponent<Transform>();
+                        float x = targetTransform.position.x - parentTransform.position.x;
+                        float y = targetTransform.position.y - parentTransform.position.y;
+                        bool isXneg = x < 0;
+                        bool isYneg = y < 0;
+                        float a = y / x;
+                        float x1 = (float)Math.Sqrt(Math.Abs(1 / (a * a + 1)));
+                        float y1 = (float)Math.Sqrt(Math.Abs(a * a / (a * a + 1)));
+                        x1 = isXneg && x1 > 0 ? -x1 : x1;
+                        y1 = isYneg && y1 > 0 ? -y1 : y1;
+                        v = new Vector3(x1, y1);
+                    }
+                parent.transform.Translate(v * 5f * Time.deltaTime);
+                //parent.transform.Translate(Vector3.down * speed * Time.deltaTime);
+                break;             case "C":                 parent.transform.Translate(Vector3.down * speed * Time.deltaTime);                 break;
             default:
                 throw new Exception("No Moving Type");
         }
@@ -38,10 +56,12 @@ public class Pattern : ScriptableObject
     public void Attack(GameObject go)
     {
         Projectile newP;
+        Projectile newP2;
+        Player target;
         switch (attackType)
         {
             case "A":
-                Player target = GameObject.FindObjectOfType<Player>();
+                target = GameObject.FindObjectOfType<Player>();
                 //플레이어가 없으면 쏘지 않음
                 if (target == null) {
                     Destroy(target); return;
@@ -89,6 +109,53 @@ public class Pattern : ScriptableObject
                         t.Translate(vector * 5f * Time.deltaTime);
                     };
                 }
+                break;
+            case "C":
+                target = GameObject.FindObjectOfType<Player>();
+                if (target == null) { Destroy(target); return; };
+                v = new Vector3(0, 0, 0);
+                newP = Instantiate(p, go.transform.position, Quaternion.identity);
+                newP.GetComponent<DirectMoving>().moveFunc = (Transform t) =>                 {
+                    if (target != null && v.Equals(new Vector3(0, 0, 0)))
+                    {
+                        /*                          항상 백터크기를 1로 하는 방법                          기울기a 는 a=(y2-y1)/(x2-x1);                          vector의 크기를 항상 n로 하기 위해서는 위 ax=y에서 x^2+y^2 = n^2이 되어야한다.                          n=1 일때를 기준으로                           Math.Sqrt = 제곱근 구하는 함수.                          x1 = (float) Math.Sqrt(Math.Abs(1/(a*a+1)));                          y1 = (float) Math.Sqrt(Math.Abs(a*a/(a*a+1)));                          이된다.                          한편 x1과 y1은 절대값이므로 초기 x와 y의 양수/음수 구분을 기록해 뒀다가 마지막에 양수음수를 맞춰줘야한다.                          */
+                        Transform targetTransform = target.GetComponent<Transform>();
+                        float x = targetTransform.position.x - t.position.x;
+                        float y = targetTransform.position.y - t.position.y;
+                        bool isXneg = x < 0;
+                        bool isYneg = y < 0;
+                        float a = y / x;
+                        float x1 = (float)Math.Sqrt(Math.Abs(1 / (a * a + 1)));
+                        float y1 = (float)Math.Sqrt(Math.Abs(a * a / (a * a + 1)));
+                        x1 = isXneg && x1 > 0 ? -x1 : x1;
+                        y1 = isYneg && y1 > 0 ? -y1 : y1;
+                        v = new Vector3(x1, y1);
+                    }
+                    t.Translate(v * 5f * Time.deltaTime);                 };
+                newP2 = Instantiate(p, go.transform.position, Quaternion.identity);
+                newP2.GetComponent<DirectMoving>().moveFunc = (Transform t) =>                 {
+                    Transform targetTransform = target.GetComponent<Transform>();
+                    float x = targetTransform.position.x - t.position.x;
+                    float y = targetTransform.position.y - t.position.y;
+                    d = (float)Math.Sqrt(0.5d);
+                    bool isXneg = x < 0;
+                    bool isYneg = y < 0;
+                    float a = y / x;
+                    float x1 = (float)Math.Sqrt(Math.Abs(1 / (a * a + 1)))+d;
+                    float y1 = (float)Math.Sqrt(Math.Abs(a * a / (a * a + 1)));
+                    x1 = isXneg && x1 > 0 ? -x1 : x1;
+                    y1 = isYneg && y1 > 0 ? -y1 : y1;
+                    v = new Vector3(x1, y1);
+                    //d = (float)Math.Sqrt(0.5d);
+                    //Vector3 v2 = new Vector3(-d, 0, 0);
+                    //v = v + v2;
+                    t.Translate(v * 5f * Time.deltaTime);
+                };
+                //float d2 = (float)Math.Sqrt(0.5d);
+                //Vector3 vectorC = new Vector3(0, 0, 0);
+                //Vector3 vectorC2 = new Vector3(d2, d2, 0);
+                //Vector3 vectorC3 = new Vector3(-d2, -d2, 0);
+                
                 break;
             case "X":
                 break;
