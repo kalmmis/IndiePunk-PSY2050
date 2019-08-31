0a0f56ca695b4ec8463d4737e487aa7dfe18d452
kalmmis <basronyu@gmail.com>
2019-08-31 13:43:36 +0900

    AddingStartMenu

--- Assets/Scripts/ApplicationManager.cs
+++ Assets/Scripts/ApplicationManager.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class ApplicationManager : MonoBehaviour {
-	
 
-	public void Quit () 
+    public void StartButtonLoadScene()
+    {
+        SceneManager.LoadScene("Demo_Scene");
+    }
+
+    public void Quit () 
 	{
 		#if UNITY_EDITOR
 		UnityEditor.EditorApplication.isPlaying = false;
