8ec44a68418e1d3c1c61c2c1eec129dade02744a
hhan87 <hhan87@gmail.com>
2019-10-31 23:08:38 +0900

    add unityAds

---
+++ Assets/Template/Scripts/AdManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+
+public class AdManager : MonoBehaviour
+{
+    public string gameId = "1234567";
+    public string placementId = "bannerPlacement";
+    public bool testMode = true;
+
+    void Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+        StartCoroutine(ShowBannerWhenReady());
+    }
+
+    IEnumerator ShowBannerWhenReady()
+    {
+        while (!Advertisement.IsReady(placementId))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.Show(placementId);
+    }
+
+}
