b5ae705744bd64b9a3a2aa6a9bc212c1761928b9
kalmmis <basronyu@gmail.com>
2021-04-23 01:55:52 +0900

    dev: 20210423
    start menu 의 프리팹까지 날렸어서 복구...

---
+++ Assets/Scripts/StoryModule.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryModule : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Image npc1    ;
+    Image npc2    ;
+    Image npc3    ;
+    Text TextBody ;
+    Text TextName;  
+
+    void Start()
+    {
+        var sscp = new StoryScriptParser();
+        sscp.test();
+        npc1 = GameObject.Find("NpcBackgroundImage1").GetComponent<Image>();
+        npc2 = GameObject.Find("NpcBackgroundImage2").GetComponent<Image>();
+        npc3 = GameObject.Find("NpcBackgroundImage3").GetComponent<Image>();
+        TextBody = GameObject.Find("StoryText").GetComponent<Text>();
+        TextName = GameObject.Find("NpcNameText").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
